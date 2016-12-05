9a12aaf6b2d04a950e0650340a532cc2587db461
LEE DONGEUN <tetares4@gmail.com>
2016-12-06 01:35:03 +0900

    무비2 씬 코딩 중

---
+++ Assets/2.Scripts/4.MovieScenes/After/Movie1Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movie1Controller : MonoBehaviour {
+
+    //CutPlayTime >> 9, 8, 8;
+
+    public GameObject Camera;
+    public List<Movie1Cut> Cut;
+    public List<Vector3> CutCameraPos;
+    public List<float> CutPlayTime;
+    public List<float> CutMoveSpeed;
+
+    private float _timer;
+    private int _cut_num;
+
+    void Start( ) {
+        _timer = 0;
+        _cut_num = 0;
+    }
+
+    void Update( ) {
+        if ( Cut.Count <= _cut_num ) {
+            return;
+        }
+        Cut[ _cut_num ].play( );
+        _timer += Time.deltaTime;
+        cameraMove( _cut_num, _cut_num + 1, CutPlayTime[ _cut_num ] );
+    }
+
+    void cameraMove( int start_cut, int end_cut, float end_time ) {
+        if ( Cut.Count <= end_cut ) {
+            return;
+        }
+        Vector3 start_pos = Cut[ start_cut ].transform.position - CutCameraPos[ start_cut ];
+        Vector3 end_pos = Cut[ end_cut ].transform.position - CutCameraPos[ end_cut ];
+        Camera.transform.position = Vector3.Lerp( start_pos, end_pos, ( _timer - end_time ) * CutMoveSpeed[ start_cut ] );
+        if ( ( _timer - end_time ) * CutMoveSpeed[ start_cut ] > 1 ) {
+            Cut[ start_cut ].stop( );
+            _cut_num++;
+            _timer = 0;
+        } 
+    }
+}
---
+++ Assets/2.Scripts/4.MovieScenes/After/Movie1Cut.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Movie1Cut : MonoBehaviour {
+    private bool _play;
+    private float _timer;
+
+    void Start( ) {
+        _play = false;
+    }
+
+    void Update( ) {
+        if ( !_play ) {
+            _timer = 0;
+            return;
+        }
+        _timer += Time.deltaTime;
+        Cut( transform.name );
+    }
+
+    void Cut( string cut_name ) {
+        switch ( cut_name ) {
+            case "Cut1":
+                GameObject.Find( "Icon1_2" ).gameObject.transform.position += new Vector3( 0.75f, 1f, 0 ) * 0.004f;
+                if ( _timer > 1f ) {
+                    GameObject.Find( "Icon1_1" ).gameObject.GetComponent<SpriteRenderer>( ).color = new Color( 255, 255, 255, _timer / 4f - 1f );
+                    GameObject.Find( "Icon1_2" ).gameObject.GetComponent<SpriteRenderer>( ).color = new Color( 255, 255, 255, 2f - _timer / 4f );
+                }
+                break;
+            case "Cut2":
+                gameObject.GetComponent<SpriteRenderer>( ).color = new Color( 255, 255, 255, _timer / 2f );
+                Color Icon2_1 = new Color( 255, 255, 255, Mathf.Sin( _timer / 4f * Mathf.PI ) );
+                Color Icon2_2 = new Color( 255, 255, 255, _timer / 2f - 2f );
+                GameObject.Find( "Icon2_1" ).gameObject.GetComponent<SpriteRenderer>( ).color = Icon2_1;
+                GameObject.Find( "Icon2_2" ).gameObject.GetComponent<SpriteRenderer>( ).color = Icon2_2;
+                break;
+            case "Cut3":
+                Vector3 Icon3_1 = new Vector3( 0, 0, 0 );
+                GameObject.Find( "Icon3_1" ).gameObject.transform.position += Icon3_1;
+                Color Icon3_2 = new Color( 255, 255, 255, Mathf.Sin( _timer / 4f * Mathf.PI ) );
+                Color Icon3_3 = new Color( 255, 255, 255, _timer / 2f - 2f );
+                GameObject.Find( "Icon3_2" ).gameObject.GetComponent<SpriteRenderer>( ).color = Icon3_2;
+                GameObject.Find( "Icon3_3" ).gameObject.GetComponent<SpriteRenderer>( ).color = Icon3_3;
+                break;
+        }
+    }
+
+    public void play( ) {
+        _play = true;
+    }
+
+    public void stop( ) {
+        _play = false;
+    }
+}
