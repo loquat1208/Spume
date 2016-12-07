dff6c19f04ce5ac51d495aa396d84f9d30e90e36
LEE DONGEUN <tetares4@gmail.com>
2016-12-08 02:14:51 +0900

    시나리오 2 완성

--- Assets/2.Scripts/4.MovieScenes/After/Movie1Controller.cs
+++ Assets/2.Scripts/4.MovieScenes/After/Movie1Controller.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movie1Controller : MonoBehaviour {
-
-    //CutPlayTime >> 9, 8, 8;
-
     public GameObject Camera;
+    public float CutMoveSpeed;
     public List<Movie1Cut> Cut;
     public List<Vector3> CutCameraPos;
     public List<float> CutPlayTime;
-    public List<float> CutMoveSpeed;
 
     private float _timer;
     private int _cut_num;
+    private int _select;
+    private int _next_cut;
 
     void Start( ) {
+        _select = PlayerPrefs.GetInt( "Select" );
         _timer = 0;
         _cut_num = 0;
+        _next_cut = 1;
     }
 
     void Update( ) {
@@ -26,7 +27,7 @@ public class Movie1Controller : MonoBehaviour {
         }
         Cut[ _cut_num ].play( );
         _timer += Time.deltaTime;
-        cameraMove( _cut_num, _cut_num + 1, CutPlayTime[ _cut_num ] );
+        cameraMove( _cut_num, _cut_num + _next_cut, CutPlayTime[ _cut_num ] );
     }
 
     void cameraMove( int start_cut, int end_cut, float end_time ) {
@@ -35,11 +36,28 @@ public class Movie1Controller : MonoBehaviour {
         }
         Vector3 start_pos = Cut[ start_cut ].transform.position - CutCameraPos[ start_cut ];
         Vector3 end_pos = Cut[ end_cut ].transform.position - CutCameraPos[ end_cut ];
-        Camera.transform.position = Vector3.Lerp( start_pos, end_pos, ( _timer - end_time ) * CutMoveSpeed[ start_cut ] );
-        if ( ( _timer - end_time ) * CutMoveSpeed[ start_cut ] > 1 ) {
+        Debug.Log( ( _timer - end_time ) * CutMoveSpeed );
+        if ( ( _timer - end_time ) * CutMoveSpeed > 0 ) {
+            Camera.transform.position = Vector3.Lerp( start_pos, end_pos, ( _timer - end_time ) * CutMoveSpeed );
+        }
+        if ( ( _timer - end_time ) * CutMoveSpeed > 1 ) {
             Cut[ start_cut ].stop( );
-            _cut_num++;
+            _cut_num += _next_cut;
+            _next_cut = 1;
             _timer = 0;
-        } 
+        }
+    }
+
+    public void Select1( ) {
+        int event_num = PlayerPrefs.GetInt( "EventNumber" );
+        _select += ( int )Mathf.Pow( 2.0f, ( float )event_num );
+        PlayerPrefs.SetInt( "Select", _select );
+        PlayerPrefs.Save( );
+        cameraMove( _cut_num, _cut_num + _next_cut, 0 );
+    }
+
+    public void Select2( ) {
+        _next_cut = 2;
+        cameraMove( _cut_num, _cut_num + _next_cut, 0 );
     }
 }
